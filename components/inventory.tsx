baac5301b8136280c8c31bc64ff5a152598936bb
Max Miller <67687590+maxtmiller@users.noreply.github.com>
2025-04-04 11:01:24 -0400

    Update inventory.tsx

--- components/inventory.tsx
+++ components/inventory.tsx
@@ -96,7 +96,7 @@ export function Inventory({ steamId }: { steamId: string | null }) {
         const data = await fetchInventory(steamId)
         const items = data.item_data
 
-        console.log("Inventory.tsx: Fetched inventory data:", items)
+        console.log("Inventory.tsx: Fetched inventory data:", data)
 
         setError(data.error)
 
